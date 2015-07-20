c4ca3aa54920d447c45003b77a04d6238d998883
dylanthelion <dillion256@gmail.com>
2015-07-20 15:12:51 -0500

    Map works

--- CityFixr/MapViewController.swift
+++ CityFixr/MapViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import GoogleMaps
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, GMSMapViewDelegate {
+    
+    @IBOutlet weak var mapContainerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        buildMap(CLLocationCoordinate2DMake(45.0, -83.0))
 
         // Do any additional setup after loading the view.
     }
@@ -20,6 +23,16 @@ class MapViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func buildMap(center : CLLocationCoordinate2D) {
+        let camera = GMSCameraPosition.cameraWithLatitude(center.latitude, longitude: center.longitude, zoom: 10)
+        
+        let myMapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        myMapView.delegate = self
+        myMapView.myLocationEnabled = true
+        myMapView.frame = mapContainerView.frame
+        view.addSubview(myMapView)
     }
     
 
